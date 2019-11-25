d8b8e40c4662de5cee20dd1008ee284e4cecfd42
moreira-matheus <moreira.matheus3692@gmail.com>
2019-11-25 18:22:39 -0300

    add section 2

---
+++ section2-numpy/dot_for.py
@@ -0,0 +1,24 @@
+import numpy as np
+from datetime import datetime
+
+a = np.random.randn(100)
+b = np.random.randn(100)
+T = 100000
+
+def slow_dot_product(a, b):
+  result = 0
+  for e, f in zip(a, b):
+    result += e*f
+  return result
+
+t0 = datetime.now()
+for t in range(T):
+  slow_dot_product(a, b)
+dt1 = datetime.now() - t0
+
+t0 = datetime.now()
+for t in range(T):
+  a.dot(b)
+dt2 = datetime.now() - t0
+
+print("dt1 / dt2:", dt1.total_seconds() / dt2.total_seconds())
